a483b3e1e0101a8649491b56bcd3bf70b3cb8e9b
alexgreenbank <alex.greenbank@grafana.com>
2022-02-23 12:24:30 +0000

    2020 day 1 part 2

--- 2020/1/1.go
+++ 2020/1/1.go
@@ -8,6 +8,32 @@ import (
 	"strconv"
 )
 
+func part1(nos map[int]bool) {
+	for k, _ := range nos {
+		n := 2020 - k
+		if _, ok := nos[n]; ok {
+			if n > k {
+				fmt.Printf("%d and %d sum to %d, product = %d\n",
+					k, n, k+n, k*n)
+			}
+		}
+	}
+}
+
+func part2(nos map[int]bool) {
+	for k1, _ := range nos {
+		for k2, _ := range nos {
+			n := 2020-k1-k2
+			if _, ok := nos[n]; ok {
+				if n > k2 && k2 > k1 {
+					fmt.Printf("%d and %d and %d sum to %d, product = %d\n",
+						k1, k2, n, k1+k2+n, k1*k2*n)
+				}
+			}
+		}
+	}
+}
+
 func main() {
 	if len(os.Args) != 2 {
 		log.Fatal("Need to supply filename as first arg")
@@ -34,12 +60,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for k, _ := range vals {
-		n := 2020 - k
-		if _, ok := vals[n]; ok {
-			if n > k {
-				fmt.Printf("%d and %d sum to %d, product = %d\n", k, n, k+n, k*n)
-			}
-		}
-	}
+	part1(vals)
+
+	part2(vals)
 }
